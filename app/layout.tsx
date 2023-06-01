84429b3aa9cd88261f6eb57cd029e2f9326b2ebc
Alisher <93440110+AlisherSrc@users.noreply.github.com>
2023-06-01 15:58:00 +0600

    add basic layout
    removed slash '/' from paths from the tsconfig for convenience

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,28 @@
-const Layout =() => {
-    return <>layout</>
+import '@styles/globals.css';
+import { ReactNode } from 'react';
+
+export const metadata = {
+    title:"PromptHub",
+    description: 'Share & Discover AI Prompts'
 }
 
-export default Layout;
+export type RootLayoutProps = {
+    children: ReactNode;
+}
+
+const RootLayout =({children} : RootLayoutProps) => {
+    return <html lang='en'>
+        <body>
+            <div className='main'>
+                <div className='gradient'/>
+            </div>
+            <main className='app'>
+                {children}
+            </main>
+        </body>
+
+    </html>
+
+}
+
+export default RootLayout;
